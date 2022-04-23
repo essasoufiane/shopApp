2941f197d8da29e05a4d2f80ea515c65d5b2bb5c
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-04-24 00:08:13 +0200

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
