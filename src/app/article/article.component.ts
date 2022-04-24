566d503960ea2187650e115f05ef7f3134613c9b
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-04-24 04:32:31 +0200

    proprietes_methodes_typesDeDonnees_filtrePipes

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-
-  constructor() { }
+// test pedagogique
+  title: string ;
+  age: number = 28;
+  isVerify: boolean = true;
+  names:string[] = ['jules','fatima','moulay'];
+  test: any = 4.5
+  date: Date = new Date()
+  produit: object[] = [
+    {
+    name: "robe pour femmes",
+    description: 'jolie robe pour femme',
+    sold_price: 1299,
+    regular_price: 1899,
+    isSolded: false,
+  },
+    {
+    name: "robe pour femmes",
+    description: 'jolie robe pour femme',
+    sold_price: 1299,
+    regular_price: 1899,
+    isSolded: false,
+  },
+    {
+    name: "robe pour femmes",
+    description: 'jolie robe pour femme',
+    sold_price: 1299,
+    regular_price: 1899,
+    isSolded: false,
+  },
+]
+// -------------
+  constructor() {
+    this.title = 'My Shop'
+  }
 
   ngOnInit(): void {
+  }
+// test pedago
+  hello():string{
+    return "2"
   }
 
 }
