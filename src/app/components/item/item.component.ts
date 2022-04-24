211b7f51cc9b1bc251f98a03e89e915ee6c9686c
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-04-24 17:54:40 +0200

    input-output_transmission-parent-enfant

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
+  @Input() papa: Product | undefined
+//undefined = soit product soit rien
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.papa);
   }
 
 }
