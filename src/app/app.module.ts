d57e2f3623b8ee151fbf288f72f9f18197f58a5e
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-04-27 01:57:39 +0200

    requetageHTTP_Bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,11 +20,12 @@ import { ModalProductViewComponent } from './components/modal-product-view/modal
     ArticleComponent,
     ItemComponent,
     ModalProductViewComponent,
-    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
