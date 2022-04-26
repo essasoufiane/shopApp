2a325ee03387d555ad44a295ffee546176dfaa11
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-04-26 20:12:06 +0200

    ajout_Promise

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -8,52 +8,59 @@ import { ProdService } from 'src/app/services/product.service';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-// test pedagogique
+  // test pedagogique
+  // age: number = 28;
+  // isVerify: boolean = true;
+  // names:string[] = ['jules','fatima','moulay'];
+  // test: any = 4.5
   title: string = 'My Shop'
-  age: number = 28;
-  isVerify: boolean = true;
-  names:string[] = ['jules','fatima','moulay'];
-  test: any = 4.5
   date: Date = new Date()
-//
-products: Product[] = [
-  // {
-  // name: "robe du desert",
-  // description:"robe de soirée",
-  // category:['robe','pantalon','jogging'],
-  // imageUrl:'https://picsum.photos/200/300',
-  // sold_price: 29,
-  // regular_price: 59,
-  // created_at: new Date(),
-  // }
-]
-// -------------
+
+  products: Product[] = [
+    // {
+    // name: "robe du desert",
+    // description:"robe de soirée",
+    // category:['robe','pantalon','jogging'],
+    // imageUrl:'https://picsum.photos/200/300',
+    // sold_price: 29,
+    // regular_price: 59,
+    // created_at: new Date(),
+    // }
+  ]
+  // -------------
   isDisplayModal: boolean = false
   modalProduct: Product | undefined
   constructor(private productService: ProdService) { }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts()
+    // this.products = this.productService.getProducts()
+    this.productService.getProducts()
+      .then((products: Product[]) => {
+        this.products = products
+      })
+      .catch(() => {
+        this.products = []
+      })
 
   }
-// test pedago
+  // test pedago
   // hello():string{
   //   return "2"
   // }
 
-  handleDeleteProduct(product: Product){
+  handleDeleteProduct(product: Product) {
     this.products = this.products.filter(p => p._id !== product._id)
-    console.log('handleDeleteProduct:',product);
+    console.log('handleDeleteProduct:', product);
   }
-  handleDisplayProductViewModal(product: Product){
+  handleDisplayProductViewModal(product: Product) {
     console.log("-------------------------------------------------")
     console.log(product)
-    if(product){
+    if (product) {
       this.isDisplayModal = true
       this.modalProduct = product
     }
   }
-  handleCloseModal(){
+  handleCloseModal() {
     this.isDisplayModal = false
     this.modalProduct = undefined
   }
